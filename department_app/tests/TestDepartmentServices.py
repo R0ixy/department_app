1ac48cc6d4b045f5e6bc3e89f55fb564529839b2
R0ixy <nikita6kom@gmail.com>
2021-11-27 12:41:43 +0200

    config change

--- department_app/tests/TestDepartmentServices.py
+++ department_app/tests/TestDepartmentServices.py
@@ -1,5 +1,5 @@
 from department_app.app import db
-from department_app.tests.base import BaseTest
+from department_app.tests.conftest import BaseTest
 from department_app.models.department_model import Department
 from department_app.service import department_service
 
--- department_app/tests/test_department.py
+++ department_app/tests/test_department.py
@@ -1,6 +1,6 @@
 from department_app.app import db
 from department_app.models.department_model import Department
-from department_app.tests.base import BaseTest
+from department_app.tests.conftest import BaseTest
 
 
 class TestDepartment(BaseTest):
