3998ce7212bbe8dcd82ab84e4d22da5f28cd7788
R0ixy <nikita6kom@gmail.com>
2021-11-27 14:13:31 +0200

    department tests

--- department_app/tests/TestDepartmentServices.py
+++ department_app/tests/TestDepartmentServices.py
@@ -13,3 +13,19 @@ class TestDepartmentServices(BaseTest):
         db.session.add(department2)
         db.session.commit()
         self.assertEqual(2, len(department_service.get_all_departments()))
+
+    def test_add_department(self):
+        department_service.add_new_department('department1', 'description')
+        self.assertEqual(1, Department.query.count())
+
+    def test_update_department(self):
+        department_service.add_new_department('department1', 'description')
+        department_service.update_department(1, 'new_department', 'new_description')
+        department = Department.query.get(1)
+        self.assertEqual('new_department', department.name)
+        self.assertEqual('new_description', department.description)
+
+    def test_delete_department(self):
+        department_service.add_new_department('department1', 'description1')
+        department_service.delete_department(1)
+        self.assertEqual(0, Department.query.count())
