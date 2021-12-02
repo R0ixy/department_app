08ae4b7ebf73b27e9b7d508104bef7f22ef5d992
R0ixy <nikita6kom@gmail.com>
2021-12-02 22:19:53 +0200

    added logging

--- department_app/app.py
+++ department_app/app.py
@@ -1,16 +1,40 @@
 """
-Starts application.
+Enable logging and start application.
 """
+import sys
+import logging
+
 from department_app.loader import app, db
 
-#
-# def create_app():
-#     app.register_blueprint(page)
-#     app.register_blueprint(rest, url_prefix='/api')
-#     return app
+# logging
+formatter = logging.Formatter(u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s')
 
+file_handler = logging.FileHandler(filename='app.log', mode='w')
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
 
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setFormatter(formatter)
+console_handler.setLevel(logging.DEBUG)
+
+# pylint: disable=no-member
+logger = app.logger
+logger.handlers.clear()
+app.logger.addHandler(file_handler)
+app.logger.addHandler(console_handler)
+app.logger.setLevel(logging.DEBUG)
+
+werkzeug_logger = logging.getLogger('werkzeug')
+werkzeug_logger.handlers.clear()
+werkzeug_logger.addHandler(file_handler)
+werkzeug_logger.addHandler(console_handler)
+werkzeug_logger.setLevel(logging.DEBUG)
+
+# sqlalchemy_logger = logging.getLogger('sqlalchemy.engine')
+# sqlalchemy_logger.addHandler(file_handler)
+# sqlalchemy_logger.addHandler(console_handler)
+# sqlalchemy_logger.setLevel(logging.DEBUG)
 if __name__ == '__main__':
     db.init_app(app)
     db.create_all()
-    app.run(debug=True, use_reloader=True)
+    app.run(use_reloader=True)
