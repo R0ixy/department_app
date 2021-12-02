d4ecfd51a860912cb2605da465d358b9eda0df70
R0ixy <nikita6kom@gmail.com>
2021-12-02 22:36:00 +0200

    additional auth tests

--- department_app/tests/test_auth_views.py
+++ department_app/tests/test_auth_views.py
@@ -46,6 +46,24 @@ class TestAuthViews(BaseTest):
                                  follow_redirects=True)
         self.assertEqual(response.status_code, http.HTTPStatus.OK)
 
+    def test_register_post_incorrect_username(self):
+        user = User(username='test', email='email', psw_hash=generate_password_hash('password'))
+        response = self.app.post('/register/',
+                                 data={'username': 'test',
+                                       'email': 'mail',
+                                       'password': 'password'},
+                                 follow_redirects=True)
+        self.assertEqual(response.status_code, http.HTTPStatus.OK)
+
+    def test_register_post_incorrect_email(self):
+        user = User(username='test', email='email', psw_hash=generate_password_hash('password'))
+        response = self.app.post('/register/',
+                                 data={'username': 'testuser',
+                                       'email': 'email',
+                                       'password': 'password'},
+                                 follow_redirects=True)
+        self.assertEqual(response.status_code, http.HTTPStatus.OK)
+
     def test_logout(self):
         response = self.app.get('/logout', follow_redirects=True)
         self.assertEqual(response.status_code, http.HTTPStatus.OK)
